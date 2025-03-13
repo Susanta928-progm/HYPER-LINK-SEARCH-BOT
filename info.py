01dc078e2b6199bd154ecd060a891344c46582e4
Susanta928-progm <susantasarkar1906@gmail.com>
2025-03-13 09:14:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 21134445
+API_HASH     = "231c18ea7273824491d6bf05425ab74e"
+BOT_TOKEN    = "7616342348:AAGrhSvjaIqgXnkqNZNAPmMc03nhkVe0zT8"
+SESSION      = "BQAzEnbIuA04WP7L6_6cA5BVZKnnI5w3A79ANqYD9OFNHFz7jASarHmAG6zbowO2J5OpxkA1J-7lT47SVtamSKOYfKkdsTs8BLHNETo-FWk3eGbT7GPjuctvpu0zXsMOJj0PzRzdYAO42W8otS6OmRwrk8xBWyDsPZetRGgj8E2F4e_A-_m3WhDbxjx2NQvUK1rxyx0oeS8nDd08BytGM6BcLeakVy99jfwf4AzMgpqPtErGrrgTEQWj9fEibG1G31nFpsfDHlq3ni8_0_ABaGZzV22Lpede34aRwRu1u3kWd4ltg1wYrABX4geWaFR7_z8Y7oonmdsKWfM3fWv1LZ34AAAAAc65fP8A"
+DATABASE_URI = "mongodb+srv://bornhyper1:Bornhyper5911@cluster0.gmvpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002166830074
+ADMIN        = 7763229951
+CHANNEL      = "@ACX_NETWORK"
